a881fb6a7dba85c3d58d3f484dd1de2575644c13
SabarishP21 <108579653+SabarishP21@users.noreply.github.com>
2022-07-02 19:10:19 +0530

    Update movies.sql

--- movies.sql
+++ movies.sql
@@ -1,4 +1,4 @@
-// Creating the table
+-- Creating the table
 CREATE TABLE movies(
     movie_name VARCHAR(50),
     lead_actor VARCHAR(50),
@@ -7,8 +7,7 @@ CREATE TABLE movies(
     director_name VARCHAR(50)
 );
 
-
-// Inserting the values into 'movies' table
+--Inserting the values into 'movies' table
 INSERT INTO movies VALUES('Bahubali The Conclusion','Prabhas','Anushka Shetty',2016,'S Rajmouli');
 INSERT INTO movies VALUES('KGF Chapter 2','Yash','Srinidhi Shetty',2022,'Prashanth Neel');
 INSERT INTO movies VALUES('Beast','Vijay','Pooja',2022,'Nelson');
@@ -16,25 +15,25 @@ INSERT INTO movies VALUES('Doctor','Sivakarthikeyan','Priyanka mohan',2021,'Nels
 INSERT INTO movies VALUES('Arjun Reddy','Vijay Devarkonda','Shalini Pandey',2017,'Sandeep Reddy');
 
 
-// 1. Retrieving all the attributes from 'movies' table
+-- 1. Retrieving all the attributes from 'movies' table
 SELECT * FROM movies;
 
 
-// 2. Retrieving all the attributes from 'movies' table based on 'lead_actor'
+-- 2. Retrieving all the attributes from 'movies' table based on 'lead_actor'
 SELECT movie_name,lead_actor,actress,year_of_release,director_name FROM movies WHERE lead_actor='Yash';
 
 
-// 3. Retrieving 'movies_name' from 'movies' table based on 'lead_actor','actress','year_of_release'
+-- 3. Retrieving 'movies_name' from 'movies' table based on 'lead_actor','actress','year_of_release'
 SELECT movie_name FROM movies WHERE lead_actor='Vijay' AND actress='Pooja' AND year_of_release=2022;
 
 
-// 4. Retrieving all the attributes from 'movies' table whose 'year_of_release' between 2017,2020
+-- 4. Retrieving all the attributes from 'movies' table whose 'year_of_release' between 2017,2022
 SELECT movie_name,lead_actor,actress,year_of_release,director_name FROM movies WHERE year_of_release BETWEEN 2017 AND 2022;
 
 
-// 5. Sort the values of tables based on 'year_of_release' 
+-- 5. Sort the values of tables based on 'year_of_release' 
 SELECT movie_name,lead_actor,actress,year_of_release,director_name FROM movies ORDER BY year_of_release;
 
 
-// 6. Retrieving all the 'movie_name','director_name' from 'movies' table that are not directed by 'Sandeep Reddy'
+-- 6. Retrieving all the 'movie_name','director_name' from 'movies' table that are not directed by 'Nelson'
 SELECT movie_name,director_name FROM movies WHERE director_name!='Nelson';
